a5dc90013f93bddd705b66fbee8edc0cee526ae9
Jess Dieuique <jessica.dieuique@gmail.com>
2021-01-09 19:34:23 -0800

    Change esc grave to grave as it wasn't working

--- keyboards/lily58/keymaps/jesstice/config.h
+++ keyboards/lily58/keymaps/jesstice/config.h
@@ -43,9 +43,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
 
-// Override Command+Option+Escape on macOS for grave escape
-#define GRAVE_ESC_GUI_OVERRIDE
-
 // Underglow
 /*
 #undef RGBLED_NUM
